d5950589685a0ec941154d098784f91b9d2a9f33
Chris Gallucci <chris@gallucci.com>
2025-11-05 15:05:55 -0600

    day 6 part 2 solution

--- internal/day06/day06.go
+++ internal/day06/day06.go
@@ -8,73 +8,138 @@ import (
 type Solver struct{}
 
 func New() *Solver {
-    return &Solver{}
+	return &Solver{}
 }
 
 type point struct{ r, c int }
 
+type state struct {
+	r, c, d int
+}
+
 var dirs = []point{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
 
+func normalize(input string) []string {
+	s := strings.TrimSpace(input)
+	if s == "" {
+		return nil
+	}
+	s = strings.ReplaceAll(s, "\r\n", "\n")
+	return strings.Split(s, "\n")
+}
+
+func findStart(grid []string) (int, int, int) {
+	for r := range grid {
+		for c := range grid[r] {
+			switch grid[r][c] {
+			case '^':
+				return r, c, 0
+			case '>':
+				return r, c, 1
+			case 'v':
+				return r, c, 2
+			case '<':
+				return r, c, 3
+			}
+		}
+	}
+	return 0, 0, 0
+}
+
 func (s *Solver) Part1(input string) string {
-    sIn := strings.TrimSpace(input)
-    if sIn == "" {
-        return "0"
-    }
-    sIn = strings.ReplaceAll(sIn, "\r\n", "\n")
-    lines := strings.Split(sIn, "\n")
+	grid := normalize(input)
+	if len(grid) == 0 {
+		return "0"
+	}
+	sr, sc, dir := findStart(grid)
 
-    grid := lines
+	visited := map[point]bool{}
+	r, c := sr, sc
+	rows := len(grid)
+	cols := len(grid[0])
 
-    var sr, sc, dir int
-    found := false
-    for r := range grid {
-        for c := range grid[r] {
-            switch grid[r][c] {
-            case '^':
-                sr, sc, dir = r, c, 0
-                found = true
-            case '>':
-                sr, sc, dir = r, c, 1
-                found = true
-            case 'v':
-                sr, sc, dir = r, c, 2
-                found = true
-            case '<':
-                sr, sc, dir = r, c, 3
-                found = true
-            }
-            if found {
-                break
-            }
-        }
-        if found {
-            break
-        }
-    }
+	for {
+		visited[point{r, c}] = true
+		nr := r + dirs[dir].r
+		nc := c + dirs[dir].c
+		if nr < 0 || nr >= rows || nc < 0 || nc >= cols {
+			break
+		}
+		ch := grid[nr][nc]
+		if ch == '#' {
+			dir = (dir + 1) % 4
+			continue
+		}
+		r, c = nr, nc
+	}
 
-    visited := map[point]bool{}
-    r, c := sr, sc
-    rows := len(grid)
-    cols := len(grid[0])
+	return strconv.Itoa(len(visited))
+}
 
-    for {
-        visited[point{r, c}] = true
-        nr := r + dirs[dir].r
-        nc := c + dirs[dir].c
-        if nr < 0 || nr >= rows || nc < 0 || nc >= cols {
-            break
-        }
-        ch := grid[nr][nc]
-        if ch == '#' {
-            dir = (dir + 1) % 4
-            continue
-        }
-        r, c = nr, nc
-    }
+func simulateLoop(grid []string, sr, sc, dir int, block point) bool {
+	rows := len(grid)
+	cols := len(grid[0])
+	seen := make(map[state]bool)
+	r, c, d := sr, sc, dir
 
-    return strconv.Itoa(len(visited))
+	for {
+		st := state{r, c, d}
+		if seen[st] {
+			return true
+		}
+		seen[st] = true
+		nr := r + dirs[d].r
+		nc := c + dirs[d].c
+		if nr < 0 || nr >= rows || nc < 0 || nc >= cols {
+			return false
+		}
+		if (nr == block.r && nc == block.c) || grid[nr][nc] == '#' {
+			d = (d + 1) % 4
+			continue
+		}
+		r, c = nr, nc
+	}
 }
 
 func (s *Solver) Part2(input string) string {
-    return ""
+	grid := normalize(input)
+	if len(grid) == 0 {
+		return "0"
+	}
+	sr, sc, dir := findStart(grid)
+
+	rows := len(grid)
+	cols := len(grid[0])
+
+	visited := make(map[point]bool)
+	r, c, d := sr, sc, dir
+
+	for {
+		visited[point{r, c}] = true
+		nr := r + dirs[d].r
+		nc := c + dirs[d].c
+		if nr < 0 || nr >= rows || nc < 0 || nc >= cols {
+			break
+		}
+		if grid[nr][nc] == '#' {
+			d = (d + 1) % 4
+			continue
+		}
+		r, c = nr, nc
+	}
+
+	count := 0
+	for p := range visited {
+		if p.r == sr && p.c == sc {
+			continue
+		}
+		if grid[p.r][p.c] == '#' {
+			continue
+		}
+		if simulateLoop(grid, sr, sc, dir, p) {
+			count++
+		}
+	}
+
+	return strconv.Itoa(count)
 }
